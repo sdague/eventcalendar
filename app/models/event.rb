d70ff5c0480ed13c0855dd69db3348d33c495bfc
Sean Dague <sean@dague.net>
2009-01-03 13:15:55 -0500

    add in validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
     belongs_to :list
+    validates_presence_of :name, :description, :location, :list
+    
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ActiveRecord::Base
     has_many :events
+    validates_presence_of :name
+    # these need to be emails
+    validates_format_of :address, :with => /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\Z/i
+    validates_format_of :from, :with => /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\Z/i
 end
