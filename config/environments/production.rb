9aab8369759980b164087f682a31cee71ce5edcc
Sean Dague <sean@dague.net>
2009-03-07 13:55:18 -0500

    fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
+config.action_mailer.delivery_method = :sendmail
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
