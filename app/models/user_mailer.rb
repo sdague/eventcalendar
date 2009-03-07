c7b3283531bb39efcd974e27f1b5e58d6d301dfc
Sean Dague <sean@dague.net>
2009-03-07 16:01:10 -0500

    fix for the event site

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,27 +3,27 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://cal.mhvlug.org/activate/#{user.activation_code}"
   
   end
   
   def reset_notification(user)
       setup_email(user)
       @subject    += 'Link to reset your password'
-      @body[:url]  = "http://www.mysite.com/reset/#{user.reset_code}"
+      @body[:url]  = "http://cal.mhvlug.org/reset/#{user.reset_code}"
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://cal.mvhlug.org/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "sean@dague.net"
+      @subject     = "[MHVLUG Mailer] "
       @sent_on     = Time.now
       @body[:user] = user
     end
