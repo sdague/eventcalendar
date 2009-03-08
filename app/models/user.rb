4171d9c0acb794acbe838869254c495567df65be
Sean Dague <sean@dague.net>
2009-03-08 08:32:50 -0400

    fix this so the mailer works right

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
 
   def create_reset_code
       @reset = true
-      self.attributes = {:reset_code => Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )}
+      self.reset_code = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
       save(false)
   end
   
