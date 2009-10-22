9cba89badae1942330d6b052cce9e9fe5d7dc5e6
Sean Dague <sean@dague.net>
2009-10-21 21:51:08 -0400

    move to just an address

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -23,7 +23,7 @@ module EventsHelper
                     dtend  e.end.to_datetime
                     summary e.name
                     description desc
-                    location "#{e.location.name}, #{e.location.address}"
+                    location "#{e.location.address}"
                     organizer     "MAILTO:#{e.list.from}"
                     klass       "PUBLIC"
                 end
